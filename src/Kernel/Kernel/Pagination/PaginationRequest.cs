1073b4cfbf0f7638574dfa0fc86a65012a9fa4fd
makampos <matheus.campos@edu.unifil.br>
2024-12-28 20:06:24 -0300

    feat: Added pagination request/response support.

---
+++ src/Kernel/Kernel/Pagination/PaginationRequest.cs
@@ -0,0 +1,3 @@
+namespace Kernel.Pagination;
+
+public record PaginationRequest(int PageIndex = 0, int PageSize = 10);
--- src/Modules/Catalog/Catalog/GlobalUsings.cs
+++ src/Modules/Catalog/Catalog/GlobalUsings.cs
@@ -19,3 +19,4 @@ global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Routing;
 global using FluentValidation;
 global using Catalog.Products.Exceptions;
+global using Kernel.Pagination;
