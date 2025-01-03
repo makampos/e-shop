0a0243476d5c4d71c3d4c2a0b373f83dcf718fbf
makampos <matheus.campos@edu.unifil.br>
2025-01-03 14:24:21 -0300

    feat: Added 'Order' entity model related value objects

---
+++ src/Modules/Ordering/Ordering/Orders/Models/Order.cs
@@ -0,0 +1,60 @@
+using Kernel.DDD;
+
+namespace Ordering.Orders.Models;
+
+public class Order : Aggregate<Guid>
+{
+    private readonly List<OrderItem> _items = [];
+    public virtual IReadOnlyList<OrderItem> Items => _items.AsReadOnly();
+
+    public Guid CustomerId { get; private set; } = default!;
+    public string OrderName { get; private set; } = default!;
+    public virtual Address ShippingAddress { get; private set; } = default!;
+    public virtual Address BillingAddress { get; private set; } = default!;
+    public virtual Payment Payment { get; private set; } = default!;
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public static Order Create(Guid id, Guid customerId, string orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment
+        };
+
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+
+        return order;
+    }
+
+    public void Add(Guid productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var existingItem = Items.FirstOrDefault(x => x.ProductId == productId);
+
+        if (existingItem != null)
+        {
+            existingItem.Quantity += quantity;
+        }
+        else
+        {
+            var orderItem = new OrderItem(Id, productId, quantity, price);
+            _items.Add(orderItem);
+        }
+    }
+
+    public void Remove(Guid productId)
+    {
+        var orderItem = _items.FirstOrDefault(x => x.ProductId == productId);
+        if (orderItem is not null)
+        {
+            _items.Remove(orderItem);
+        }
+    }
+}
