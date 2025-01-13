62a6e4eb866f40793f2a98edc91cf9820e8c9362
makampos <matheus.campos@edu.unifil.br>
2025-01-12 21:48:28 -0300

    feat: Added remaining tests for creating product

--- src/Api.Integration.Tests/Modules/Catalog/Products/Features/CreateProductHandlerTests.cs
+++ src/Api.Integration.Tests/Modules/Catalog/Products/Features/CreateProductHandlerTests.cs
@@ -47,4 +47,61 @@ public class CreateProductHandlerTests : BaseIntegrationTest
         exception.Errors.Should().HaveCount(1);
         exception.Errors.First().ErrorMessage.Should().Be("Name is required");
     }
+
+    [Fact(DisplayName = "Create Product Command with Invalid Price Should Throw Validation Exception")]
+    public async Task CreateProductCommand_InvalidPrice_ThrowsValidationException()
+    {
+        var command =
+            new CreateProductCommand(
+                new ProductDto(
+                    Guid.Empty,
+                    "Playstation 5",
+                    ["Consoles"],
+                    "Some Description",
+                    ".png",
+                    0));
+
+        var exception = await Assert.ThrowsAsync<ValidationException>(async () => await Sender.Send(command));
+
+        exception.Errors.Should().HaveCount(1);
+        exception.Errors.First().ErrorMessage.Should().Be("Price must be greater than 0");
+    }
+
+    [Fact(DisplayName = "Create Product Command with Invalid Category Should Throw Validation Exception")]
+    public async Task CreateProductCommand_InvalidCategory_ThrowsValidationException()
+    {
+        var command =
+            new CreateProductCommand(
+                new ProductDto(
+                    Guid.Empty,
+                    "Playstation 5",
+                    [],
+                    "Some Description",
+                    ".png",
+                    399));
+
+        var exception = await Assert.ThrowsAsync<ValidationException>(async () => await Sender.Send(command));
+
+        exception.Errors.Should().HaveCount(1);
+        exception.Errors.First().ErrorMessage.Should().Be("Category is required");
+    }
+
+    [Fact(DisplayName = "Create Product Command with Invalid Image File Should Throw Validation Exception")]
+    public async Task CreateProductCommand_InvalidImageFile_ThrowsValidationException()
+    {
+        var command =
+            new CreateProductCommand(
+                new ProductDto(
+                    Guid.Empty,
+                    "Playstation 5",
+                    ["Consoles"],
+                    "Some Description",
+                    string.Empty,
+                    399));
+
+        var exception = await Assert.ThrowsAsync<ValidationException>(async () => await Sender.Send(command));
+
+        exception.Errors.Should().HaveCount(1);
+        exception.Errors.First().ErrorMessage.Should().Be("ImageFile is required");
+    }
 }
